2d9cd2c598cd3c3fc70b1c64b727258ceee74e86
Maham Waseem <mahamw440@gmail.com>
2025-06-26 19:22:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const { URL } = require('url');
 let notes = [];
 let nextId = 1;
 
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const method = req.method;
+
+  // GET /notes
+  if (method === 'GET' && url.pathname === '/notes') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(notes));
+  }
+});
+
