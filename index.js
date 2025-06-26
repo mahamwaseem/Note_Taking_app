78e49b8e7e63b2653aa5bfa559b42932e4f90d11
Maham Waseem <mahamw440@gmail.com>
2025-06-26 19:24:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,18 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(notes));
   }
+
+   else if (method === 'POST' && url.pathname === '/notes') {
+    let body = '';
+    req.on('data', chunk => body += chunk);
+    req.on('end', () => {
+      const { title, content } = JSON.parse(body);
+      const note = { id: nextId++, title, content };
+      notes.push(note);
+
+      res.writeHead(201, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(note));
+    });
+  }
 });
 
